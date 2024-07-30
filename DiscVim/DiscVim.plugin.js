fcbd3b0a59a2f35b4bdb5361ae81143460205123
sebvu <santosjester738@gmail.com>
2024-07-30 13:45:38 -0500

    Flesh out existing logic

--- DiscVim/DiscVim.plugin.js
+++ DiscVim/DiscVim.plugin.js
@@ -6,11 +6,12 @@
  */
 
 function collectClickableElements() {
-  /*
-    Collect all clickable elements within viewport
+  /**
+   * Collects all clickable elements within viewport
+   *
+   * @return NodeList of all elements queried
+   */
 
-    @return NodeList of all elements queried
-  */
   return document.querySelectorAll(`
     a[href], 
     button, 
@@ -27,11 +28,95 @@ function collectClickableElements() {
   `);
 }
 
+function charPair(elementMap, element) {
+  /**
+   * Assign value pair
+   *
+   * @return Unique Letter Combination
+   * @param elements is a NodeList of all clickable elements
+   */
+
+  const chars = [
+    "A",
+    "S",
+    "D",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "W",
+    "E",
+    "R",
+    "U",
+    "I",
+    "O",
+  ];
+
+  while (true) {
+    let pair =
+      chars[Math.floor(Math.random() * chars.length)] +
+      chars[Math.floor(Math.random() * chars.length)];
+
+    if (elementMap.get(pair) == undefined) {
+      return pair;
+    }
+  }
+}
+
+function sidePosition(element) {
+  /**
+   * Determine tooltip side position based on element
+   *
+   * @param element is a node
+   * @return side for tooltip to lean on
+   */
+
+  if (element.getBoundingClientRect().x < window.innerWidth / 2) {
+    return "right";
+  } else {
+    return "left";
+  }
+}
+
+function uniqueCombinator(elements) {
+  /**
+   * Assign unique letter combinations on tooltips of all elements
+   *
+   * @param elements is a NodeList of all clickable elements
+   */
+
+  let uniquePairs = new Map();
+
+  for (const element of elements) {
+    uniquePairs.set(element, charPair(uniquePairs, element));
+
+    element.style.backgroundColor = "blue";
+    BdApi.UI.createTooltip(element, uniquePairs.get(element), {
+      style: "info",
+      side: sidePosition(element),
+    });
+  }
+}
+
+function errorMessage(message) {
+  // Helper errorMessage function
+
+  BdApi.UI.showToast(message, {
+    type: "error",
+    timeout: 5000,
+  });
+}
+
 function clearUI() {
   // Clear currently applied UI
 
-  const clickableElements = collectClickableElements();
-  for (const element of clickableElements) {
+  // const currentNodes = collectClickableElements();
+
+  const currentNodes = collectClickableElements();
+
+  for (const element of currentNodes) {
     element.style.backgroundColor = "transparent";
   }
 }
@@ -40,64 +125,80 @@ module.exports = class DiscVim {
   constructor(meta) {
     // bind class instance to DiscVIm
     this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.applied = false.bind;
+    this.applied = false;
   }
 
   handleKeyDown(event) {
-    switch (event.key.toLowerCase()) {
-      case "f":
-        /*
+    try {
+      switch (event.key.toLowerCase()) {
+        case "f":
+          /*
           Pull all clickable elements within viewport, add unique letter combination to each one.
           All other controls are disabled until case key is pressed again, or combination is satisfactory.
         */
-        if (!this.applied) {
-          const clickableElements = collectClickableElements();
-          for (const element of clickableElements) {
-            element.style.backgroundColor = "blue";
+          if (this.applied) {
+            clearUI();
+            this.applied = false;
+          } else {
+            const currentNodes = collectClickableElements();
+            uniqueCombinator(currentNodes);
+            this.applied = true;
           }
-          this.applied = true;
-        } else {
-          clearUI();
-          this.applied = false;
-        }
-        break;
-      case "k":
-        /*
+          console.log(this.applied);
+          break;
+        case "k":
+          /*
           Scroll Up
         */
-        BdApi.UI.showToast("k (up) is not binded yet!", {
-          type: "warning",
-          timeout: 1000,
-        });
-        break;
-      case "j":
-        /*
+          BdApi.UI.showToast("k (up) is not binded yet!", {
+            type: "warning",
+            timeout: 1000,
+          });
+          break;
+        case "j":
+          /*
           Scroll Down
         */
-        BdApi.UI.showToast("j (down) is not binded yet!", {
-          type: "warning",
-          timeout: 1000,
-        });
-        break;
-      case "u":
-        /*
+          BdApi.UI.showToast("j (down) is not binded yet!", {
+            type: "warning",
+            timeout: 1000,
+          });
+          break;
+        case "u":
+          /*
           Scroll Half Page Up
         */
-        BdApi.UI.showToast("u (half page up) is not binded yet!", {
-          type: "warning",
-          timeout: 1000,
-        });
-        break;
-      case "d":
-        /*
+          window.scrollBy(0, -100);
+          BdApi.UI.showToast("u (half page up) is not binded yet!", {
+            type: "warning",
+            timeout: 1000,
+          });
+          break;
+        case "d":
+          /*
           Scroll Half Page Down
         */
-        BdApi.UI.showToast("d (half page down) is not binded yet!", {
-          type: "warning",
-          timeout: 1000,
-        });
-        break;
-      default:
+          BdApi.UI.showToast("d (half page down) is not binded yet!", {
+            type: "warning",
+            timeout: 1000,
+          });
+          break;
+        case "g":
+          var objDiv = document.getElementById("divExample");
+          objDiv.scrollTop = objDiv.scrollHeight;
+
+          BdApi.UI.showToast("Scrolling to bottom of page", {
+            type: "info",
+            timeout: 1000,
+          });
+          break;
+        default:
+      }
+    } catch (err) {
+      errorMessage("DiscVim failed to handle keydown, check console!");
+      console.error("DiscVim Keydown Error:", err.message);
+    } finally {
+      stop();
     }
   }
 
@@ -115,10 +216,7 @@ module.exports = class DiscVim {
       );
       console.log("DiscVim Successfully Initialized");
     } catch (error) {
-      BdApi.UI.showToast("DiscVim error, check console!", {
-        type: "error",
-        timeout: 5000,
-      });
+      errorMessage("DiscVim failed to start, check console!");
       console.error("DiscVim Failed:", error.message);
     } finally {
       stop();
@@ -137,10 +235,7 @@ module.exports = class DiscVim {
       // Successful cleanup
       console.log("DiscVim Successfully Cleaned");
     } catch (error) {
-      BdApi.UI.showToast("DiscVim failed to cleanup, check console!", {
-        type: "error",
-        timeout: 5000,
-      });
+      errorMessage("DiscVim failed to cleanup, check console!");
       console.error("DiscVim Cleanup Failure:", error.message);
     } finally {
       window.stop();
