e3297027991c0cefab9bc0080d630c63976d3adc
sebvu <santosjester738@gmail.com>
2024-07-29 23:58:31 -0500

    Add basic alert upon plugin startup

---
+++ DiscVim/DiscVim.plugin.js
@@ -0,0 +1,17 @@
+/**
+ * @name DiscVim
+ * @author Sebvu
+ * @description Allows the integration of vim motions into Discord with customizable features.
+ * @version 0.0.1
+ */
+
+module.exports = class DiscVim {
+  start() {
+    BdApi.alert("goodbye world!", "bye");
+    // Do stuff when enabled
+  }
+
+  stop() {
+    // Cleanup when disabled
+  }
+};
